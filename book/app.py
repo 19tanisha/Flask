e6d66f416178a8328b4e6bd3e36d17847d1639dd
19tanisha <tanishavanneri19@gmail.com>
2021-04-30 17:31:56 +0530

    Created variables using flask

---
+++ book/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for 
+app = Flask(__name__)
+@app.route('/index/<username>')
+def index(username):
+    return 'Welcome %s'%username
+
+@app.route('/admin')
+def admin():
+    return 'welcome admin'
+
+@app.route('/profile/<name>')
+def profile(name):
+    if name == 'admin':
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('index', username=name))
+app.run()
